9c94d6a1a31c375a045279949abaef6b0218de3f
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-01-23 19:03:04 -0400

    Initial Commit

---
+++ BitPrice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitPrice
+//
+//  Created by Maxwell Silva on 23/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
