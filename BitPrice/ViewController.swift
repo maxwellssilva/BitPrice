86108f5e311eeec2b0838dbc275437b161428242
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-01-23 21:19:28 -0400

    Testando a URL e requisição

--- BitPrice/ViewController.swift
+++ BitPrice/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://www.blockchain.com/pt/ticker") {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if error == nil {
+                    print("Sucesso! URL funcionando corretamente")
+                    if let dataReturn = data {
+                        do {
+                            if let objectJson = try JSONSerialization.jsonObject(with: dataReturn, options: [])
+                                as? [String: Any] {
+                                if let brl = objectJson["BRL"] as? [String: Any] {
+                                    if let buyBitcoin = brl["buy"] as? Double {
+                                        print(buyBitcoin)
+                                    }
+                                }
+                                //print(objectJson)
+                            }
+                        } catch {
+                            print("Erro ao formatar o objeto JSON")
+                        }
+                    }
+                } else {
+                    print("Erro ao consultar a URL")
+                }
+            }
+            task.resume()
+        }
     }
-
-
 }
 
